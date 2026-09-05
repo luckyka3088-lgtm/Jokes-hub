64b0f72e4ea57b2d4052f4236c685fd96d197269
Lucky kumar <luckyka3088@gmail.com>
2026-09-05 16:45:04 +0530

    Refactor askGemini function with new API integration

--- gemini.js
+++ gemini.js
@@ -1,4 +1,4 @@
-async function askGemini(Text) {
+/*async function askGemini(Text) {
   try {
     const response = await fetch("https://jokes-hub-beta.vercel.app/api/gemini.js", {
       method: "POST",
@@ -23,4 +23,61 @@ async function askGemini(Text) {
 
     return "Sorry, something went wrong. Please try again.";
   }
+}
+*/
+
+const API_KEY = "AQ.Ab8RN6KgZLQDA8AZAXZYPXyXt-avd-ffhLh65I17QpSlFxUoWA";
+
+async function askGemini(Text) {
+    const controller = new AbortController();
+    
+    const timeout = setTimeout(() => {
+        controller.abort();
+    }, 15000);
+
+    
+    try {
+        const response = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent?key=${API_KEY}`,
+            {
+                method: "POST",
+
+                headers: {
+                    "Content-Type": "application/json"
+                },
+
+                body: JSON.stringify({
+                    contents: [
+                        {
+                            parts: [
+                                {
+                                    text: Text
+                                }
+                            ]
+                        }
+                    ]
+                }),
+
+                signal: controller.signal
+            }
+        );
+
+        clearTimeout(timeout);
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(
+                data.error?.message || "API request failed"
+            );
+        }
+
+        return data.candidates[0].content.parts[0].text;
+
+    } catch (error) {
+
+        clearTimeout(timeout);
+
+        return "Error: " + error.message;
+    }
 }
