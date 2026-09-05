10dc2ed61f1c3d66d4abb3ae846b2c848c1240a9
Lucky kumar <luckyka3088@gmail.com>
2026-09-05 15:59:23 +0530

    Update gemini.js

--- gemini.js
+++ gemini.js
@@ -1,6 +1,6 @@
 async function askGemini(Text) {
   try {
-    const response = await fetch("/api/gemini", {
+    const response = await fetch("jokes-hub-ten.vercel.app/api/gemini", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
